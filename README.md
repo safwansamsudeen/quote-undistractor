4904780f26bf29ddf3dd15bc98e60062b8d01f7c
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2022-12-12 05:09:41 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+## A Boost for reducing distraction
+
+How many times were you been working, went onto Google and never came off? Or when you're supposed to be with family, you're staring at the screen? For the vast majority of us, such occurrences are only too frequent. In the journey of trying to combat this problem, we start off with trying to improve "self control", and when that failed, we tried different extensions, slowly increasing in harshness - we might even have an extension which blocks us from Googling during work time.
+
+With this Boost, I've tried to solve the problem in a much less severe, and hopefully more efficient way. Quote Undistractor's merely shows you a quote whenever you browse, along with some statistics. It does't stop you browsing. It doesn't make you wait for five seconds while you twiddle your thumbs impatiently. The quote is a random one, with topics based on the time of the day - customizable by the user.
+
+The idea behind Quote Undistractor is simple but powerful - to reminding you (indirectly) of your digital minimalism mindset right in the moment of weakness. Let's say you're with your kids, and you start browsing. Seeing a quote on the importance of good parenting is bound to jerk you back the children. While working, you're not gonna read up on last night's FIFA match if you've just seen what Steve Jobs thought about working hard. You'll focus better on your lunch if trying to browse resulted in you learning about the importance of food.
+
+With almost 125 topics, you can set a list of topics from which quotes would appear for each "period" of your typical day. For example, let's say I want to work from 8 to 12 morning, and 2 to 6 evening. I'd might set that time period to topics from motivational (ID 75) and technology (ID 112). The IDs for the topics can be found in `topics.json`. Read the installation instructions to know how to configure them.
+
+As an additional push in case your conscience manages to defend the first blow, the Boost also display how much times you've visited Google on that day and how many minutes you've spent on it. After you actually search for something, you'll only see a clipped portion of the quote along with the statistics. I hope this will encourage curious users to click the clipped portion, which will bring them back the the Google search page and allow them to read the whole quote. And then log off :).
+
+For more details, watch a video at https://app.box.com/s/868otooovb86u6lagl6scwxgbocs666q.
+
+## Installation
+
+1. Download and add the Boost to Arc
+2. Subscribe for the [Quotel - Quotes API](https://rapidapi.com/skjaldbaka17/api/quotel-quotes) (by clicking on the subscribe button) after creating a free RapidAPI account. I wish I could remove all this API stuff as it complicates the installation process, but my plan is a Free one and I have access to only a 100 API calls everyday. If you subscribe to Quotel - Quotes (it's completely free) you too will get to access to 100 calls (that is, 100 quotes) every day.
+3. Copy the X-RapidAPI-Key, and paste it into the value of the `API_KEY` in line 2 of `content.js`
+4. Open `topics.json`, where there's a list of topics along with their IDs. Choose the topics you want and copy over their IDs.
+5. Modify the variables in `content.js` to reflect your choice of topics and periods. Add, modify, or remove periods as you like. For each period, put all the IDs in square brackets after a colon after your chosen name of the period. The IDs should be seperated with a comma, and each line should end with a common. for example, here, for the "work" period, quotes from the topic 75 will appear on the Google homepage.
+
+```js
+const TOPICS = {
+  work: [75],
+  children: [79, 54],
+  lunch: [23, 96],
+  browsing: [91, 112],
+};
+```
+
+6. Set the timings for the periods. For each period, set the timings in square brackets after a colon after your chosen name of the period. Each timing is represented by `[startTime, endTime]`. The starting and ending times should be in hours in the 24 hour clock. The timings should be seperated with a comma, and each line should end with a common. For example, this will result in the work period being set to 8 AM to 12 PM, along with 2 to 6 PM. The children period is 7 to 8 PM.
+
+```js
+const PERIODS = {
+  work: [
+    [8, 12],
+    [14, 20],
+  ],
+  children: [[21, 22]],
+  lunch: [[13, 14]],
+  browsing: [[6, 7]],
+};
+```
+
+7. Feel free to raise a pull request if there are any errors or you have any doubts!
---
+++ content.js
@@ -0,0 +1,177 @@
+/* HEADS UP: This is the section you should modify - read the instructions for more detail */
+const API_KEY = ""; // Set the API key here
+
+const TOPICS = {
+  work: [75],
+  children: [79, 54],
+  lunch: [23, 96],
+  browsing: [91, 112],
+};
+
+const PERIODS = {
+  children: [
+    [8, 12],
+    [14, 20],
+  ],
+  work: [[21, 22]],
+  lunch: [[13, 14]],
+  browsing: [[6, 7]],
+};
+/* END OF USER MODIFICATION */
+
+/* Helper functions */
+const setNextQuote = async () => {
+  // Download a random quote from the API based on options
+  const newQuoteRes = await fetch(
+    "https://quotel-quotes.p.rapidapi.com/quotes/random",
+    options
+  );
+  const newQuote = await newQuoteRes.json();
+  // Set to LocalStorage
+  if (!newQuote.quote) {
+    alert("An error occured, please check the console");
+    console.log(newQuote);
+    return;
+  }
+  localStorage.setItem("quote", newQuote.quote);
+  localStorage.setItem("quote-author", newQuote.name);
+};
+
+function getDivider(quote) {
+  // Divide a quote by the first word break after the 25th character
+  let i = 25;
+
+  let c = quote[i];
+  while (![" "].includes(c) && i < quote.length) {
+    c = quote[i + 1];
+    i++;
+  }
+
+  let clippedQuote = quote.slice(0, i);
+  let rest = quote.slice(i, quote.length);
+
+  return [clippedQuote, rest];
+}
+
+const getStatsStatement = () => {
+  if (onHomePage) {
+    return `You've visited <span class="standout">${visitCount}</span> time${
+      visitCount !== 1 ? "s" : ""
+    } today, spending <span class="standout">${visitTime}</span> minute${
+      visitTime !== "1.00" ? "s" : ""
+    }. Was it really worth it?`;
+  } else {
+    return `<span class="standout">${visitCount}</span>t, <span class="standout">${visitTime}</span>m`;
+  }
+};
+
+const getTopics = () => {
+  let h = now.getHours();
+
+  let periods = [];
+  for (let [period, time] of Object.entries(PERIODS)) {
+    for (hourPeriod of time) {
+      if (hourPeriod[0] <= h && h < hourPeriod[1]) {
+        periods.push(period);
+      }
+    }
+  }
+
+  let topicIds = [];
+  for (let period of periods) {
+    topicIds = topicIds.concat(TOPICS[period]);
+  }
+  return JSON.stringify(topicIds);
+};
+
+/* Functionality */
+// Set visit constants
+const onHomePage = ["/", "/webhp"].includes(location.pathname);
+const now = new Date();
+
+// Set up topics
+const topicIds = getTopics();
+const options = {
+  method: "POST",
+  headers: {
+    "content-type": "application/json",
+    "X-RapidAPI-Key": API_KEY,
+    "X-RapidAPI-Host": "quotel-quotes.p.rapidapi.com",
+  },
+  body: `{"topicIds":${topicIds}}`,
+};
+
+// Get quote
+const quote = localStorage.getItem("quote") || "";
+const author = localStorage.getItem("quote-author") || "";
+const [clippedQuote, rest] = getDivider(quote);
+
+// and add it to DOM
+let logo = document.querySelector("img");
+// fix a bug related to when a Google Doodle would appear instead of the logo
+if (logo.alt !== "Google") {
+  logo = logo.parentElement.parentElement;
+}
+contentHolder = document.createElement("div");
+contentHolder.classList.add("clipped-quote");
+contentHolder.textContent = clippedQuote;
+logo.parentElement.append(contentHolder);
+
+if (!onHomePage) {
+  contentHolder.innerHTML = `<a href='https://google.com'>${contentHolder.textContent}</a>`;
+}
+
+// Display rest of the quote only if on home page
+if (onHomePage) {
+  let visibleCenter = document.querySelectorAll("center")[1];
+  restHolder = document.createElement("div");
+  restHolder.classList.add("rest");
+  restHolder.textContent = rest;
+  visibleCenter.prepend(restHolder);
+}
+
+// Get statistics
+let dayId = Math.floor(now / 8.64e7);
+
+const day = parseInt(localStorage.getItem("last-day"));
+let visitCount = 0;
+let visitTime = 0.0;
+if (day && day === dayId) {
+  visitCount = parseInt(localStorage.getItem("visit-count")) || 0;
+  visitTime =
+    parseFloat(localStorage.getItem("visit-time")).toFixed(2) || "0.00";
+}
+
+// Report stats
+let stats = document.createElement("div");
+stats.classList.add("stats");
+stats.innerHTML = getStatsStatement();
+
+if (onHomePage) {
+  stats.innerHTML += `<div class="author">Quote from ${author}</div>`;
+  logo.parentElement.parentElement.prepend(stats);
+} else {
+  stats.classList.add("stats-small");
+  holder = document.createElement("div");
+  holder.append(stats);
+  holder.append(contentHolder);
+  console.log(holder, logo);
+  logo.parentElement.prepend(holder);
+  logo.remove();
+}
+
+/* Work for next visit */
+// Update stats before window unloads
+window.onbeforeunload = () => {
+  localStorage.setItem("last-day", dayId);
+  localStorage.setItem("visit-count", visitCount + 1);
+  localStorage.setItem(
+    "visit-time",
+    parseFloat(visitTime) + (Date.now() - now) / 60000
+  );
+};
+
+// Download next quote asynchronously if user has read this quote
+if (onHomePage) {
+  setNextQuote();
+}
